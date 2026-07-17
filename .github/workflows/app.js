cbeefb4f25a106201029c370c431ca5ae37accfc
Phạm Nguyễn Anh Kiệt <kietph2110@gmail.com>
2026-07-18 02:37:58 +0700

    fix host

---
+++ .github/workflows/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Lấy version từ biến môi trường APP_VERSION, mặc định là v1.0 
+const VERSION = process.env.APP_VERSION || 'v1.0';
+
+// Endpoint GET / trả về tên app và version 
+app.get('/', (req, res) => {
+  res.send(`<h1>Hello Gitops- Test Auto CI/CD Thành Công!</h1><p>App Name: Demo App</p><p>Version: ${VERSION}</p>`);
+});
+
+// Endpoint GET /health trả về {status: ok} 
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'ok' });
+});
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`App listening at http://0.0.0.0:${port}`);
+
+  // Cố tình chèn lỗi Runtime để phá sập tiến trình Node.js
+  //throw new Error("Crash App - Mo phong loi he thong!");
+  
+});
+//TEST CI FLOW
